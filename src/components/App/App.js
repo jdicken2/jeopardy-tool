fd3ddb1c5e868715ab5e4928c072bd40c828b131
jdicken2 <jdicken2@hawk.iit.edu>
2020-04-15 20:11:59 -0500

    Added Link to Creation Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,4 +24,5 @@ export default function App() {
             <Main />
         </ThemeProvider>
     );
+    //God left me unfinished 
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'; 
 export default function Home() {
     return (
         <div>
-            Hello Jeopardy Tool!
+           <center> Hello Jeopardy Tool! </center>
+           <center>Here is the   <Link to="/Create"> link </Link>to the Creation Tool: </center>
         </div>
+
+        //Link to github footer
+        //Features 
+        //Coming soon
+        
     );
 }
